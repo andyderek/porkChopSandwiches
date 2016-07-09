4494e2bc373479214bfe913ec74654e78426877b
Andy Nixon <andielane@yahoo.com>
2016-07-09 13:48:07 -0700

    Playing with some DOM manipulations.

--- DOMManip/domManip.js
+++ DOMManip/domManip.js
@@ -1,1 +1,9 @@
-document.querySelector('div');
+window.onload = function(){
+	var myTag = document.createElement("my-tag");
+	myTag.addEventListener('onclick', function(e){
+		alert('You just Clicked me');
+	});
+	myTag.innerHTML = "Pork Chop Sandwiches";
+	myTag.onclick = function(){alert("What Just Happened")};
+	document.body.appendChild(myTag);
+};
