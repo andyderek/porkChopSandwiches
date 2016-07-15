6aa78b7a3b97490d2567c7cbe106b0e39d0eba00
Andy Nixon <andielane@yahoo.com>
2016-07-15 14:44:15 -0700

    Playing with Buttons

--- DOMManip/domManip.js
+++ DOMManip/domManip.js
@@ -3,14 +3,30 @@ window.onload = function(){
 	// myTag.addEventListener('onclick', function(e){
 	// 	alert('You just Clicked me');
 	// });
-	for(var i = 0; i <= 10; i++){
-		var myTag = document.createElement("my-tag" + i);
-		// myTag.onclick = function(){alert("What Just Happened" + i)};
-		myTag.innerHTML = "<button>" + i +"</button>";
-		myTag.onclick = function(){};
-		document.body.appendChild(myTag);
+	var numArr = [];
+	for(var i = 1; i <= 10; i++){
+		numArr.push(document.createElement("my-tag" + i));
 	}
-	console.log(myTag)
+
+	for(var i = 0; i < numArr.length; i++){
+		numArr[i].innerHTML = "<button>" + (i + 1) + "</button>";
+		document.body.appendChild(numArr[i])
+		numArr[i].onclick = function(){
+			alert(this.innerText)
+		}
+	}
+
+	console.log(numArr)
+
+	// for(var i = 0; i < numArr.length; i++){
+	// 	console.log(numArr[i].innerText)
+	// 	numArr[i].onclick = function(){
+	// 		alert(this.innerText)
+	// 	};
+	// }
+
+
+	// console.log(document.body.childNodes)
 	function count(number){
 		alert(number)
 	}
