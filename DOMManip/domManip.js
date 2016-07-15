d6a664fe2278cc62d072a2daa81ab530b965cdd0
Andy Nixon <andielane@yahoo.com>
2016-07-15 13:58:33 -0700

    Creating custom buttons with dom manipulation.

--- DOMManip/domManip.js
+++ DOMManip/domManip.js
@@ -1,9 +1,18 @@
 window.onload = function(){
-	var myTag = document.createElement("my-tag");
-	myTag.addEventListener('onclick', function(e){
-		alert('You just Clicked me');
-	});
-	myTag.innerHTML = "Pork Chop Sandwiches";
-	myTag.onclick = function(){alert("What Just Happened")};
-	document.body.appendChild(myTag);
+	// var myTag = document.createElement("my-tag");
+	// myTag.addEventListener('onclick', function(e){
+	// 	alert('You just Clicked me');
+	// });
+	for(var i = 0; i <= 10; i++){
+		var myTag = document.createElement("my-tag" + i);
+		// myTag.onclick = function(){alert("What Just Happened" + i)};
+		myTag.innerHTML = "<button>" + i +"</button>";
+		myTag.onclick = function(){};
+		document.body.appendChild(myTag);
+	}
+	console.log(myTag)
+	function count(number){
+		alert(number)
+	}
+
 };
