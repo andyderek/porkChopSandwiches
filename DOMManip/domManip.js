3aeee4c57745ef49dadf0e5673f0085ac4956257
Andy Nixon <andielane@yahoo.com>
2016-07-15 14:45:59 -0700

    Removing extra code

--- DOMManip/domManip.js
+++ DOMManip/domManip.js
@@ -1,8 +1,5 @@
 window.onload = function(){
-	// var myTag = document.createElement("my-tag");
-	// myTag.addEventListener('onclick', function(e){
-	// 	alert('You just Clicked me');
-	// });
+
 	var numArr = [];
 	for(var i = 1; i <= 10; i++){
 		numArr.push(document.createElement("my-tag" + i));
@@ -14,21 +11,6 @@ window.onload = function(){
 		numArr[i].onclick = function(){
 			alert(this.innerText)
 		}
-	}
-
-	console.log(numArr)
-
-	// for(var i = 0; i < numArr.length; i++){
-	// 	console.log(numArr[i].innerText)
-	// 	numArr[i].onclick = function(){
-	// 		alert(this.innerText)
-	// 	};
-	// }
-
-
-	// console.log(document.body.childNodes)
-	function count(number){
-		alert(number)
 	}
 
 };
